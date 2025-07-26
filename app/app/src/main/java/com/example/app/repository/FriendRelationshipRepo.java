64bb859da1b246398ab6793b416b59cc6e1277e0
thanhle2004 <yatomisuo@gmail.com>
2025-07-26 13:08:07 +0700

    Update homepage UI layout

--- app/app/src/main/java/com/example/app/repository/FriendRelationshipRepo.java
+++ app/app/src/main/java/com/example/app/repository/FriendRelationshipRepo.java
@@ -5,11 +5,8 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.example.app.entity.FriendRelationship;
-import com.example.app.entity.User;
 
 public interface FriendRelationshipRepo extends JpaRepository<FriendRelationship, Integer>{
     List<FriendRelationship> findByFirstUser_UserIdOrSecondUser_UserId(int firstId, int secondId);
