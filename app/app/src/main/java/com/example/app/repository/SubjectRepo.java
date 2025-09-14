390c4a2a63c9351885ef1e860b0c6b2b2dbab0ef
thanhle2004 <yatomisuo@gmail.com>
2025-09-14 11:01:01 +0700

    Small fix

--- app/app/src/main/java/com/example/app/repository/SubjectRepo.java
+++ app/app/src/main/java/com/example/app/repository/SubjectRepo.java
@@ -11,14 +11,14 @@ import com.example.app.entity.Subject;
 public interface SubjectRepo extends JpaRepository<Subject, Integer> {
 
     @Query("SELECT s FROM Subject s WHERE s.subject_id NOT IN " +
-           "(SELECT sr.subject.subject_id FROM SubjectRegister sr WHERE sr.userCurriculum.userCurriculumId = :userId)")
+           "(SELECT sr.subject.subject_id FROM SubjectRegister sr WHERE sr.userCurriculum.user.userId = :userId)")
     List<Subject> findUnregisteredSubjectsByUserId(@Param("userId") int userId);
 
     @Query("SELECT s FROM Subject s JOIN SubjectRegister sr ON s.subject_id = sr.subject.subject_id " +
-           "WHERE sr.userCurriculum.userCurriculumId = :userId AND sr.overall = 'studying'")
+           "WHERE sr.userCurriculum.user.userId = :userId AND sr.overall = 'studying'")
     List<Subject> findInProgressSubjectsByUserId(@Param("userId") int userId);
 
     @Query("SELECT s FROM Subject s JOIN SubjectRegister sr ON s.subject_id = sr.subject.subject_id " +
-           "WHERE sr.userCurriculum.userCurriculumId = :userId AND sr.overall IN ('pass', 'fail')")
+           "WHERE sr.userCurriculum.user.userId = :userId AND sr.overall IN ('pass', 'fail')")
     List<Subject> findCompletedSubjectsByUserId(@Param("userId") int userId);
 }
